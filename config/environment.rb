47e6ea2dd0e1463fecdb196e00e79b353baa1295
Carl Timm <ctiv@hotmail.com>
2014-04-15 11:20:59 -0700

    Removed extra 'annotate' gem from gemfile and added ignore sqlite3 to gitignore

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Web::Application.initialize!
 
 # Fixes SSL Connection Error in Windows execution of Ruby
 # Based on fix described at: https://gist.github.com/fnichol/867550
-ENV['SSL_CERT_FILE'] = File.expand_path(File.dirname(__FILE__)) + "/config/cacert.pem"
+ENV['SSL_CERT_FILE'] = File.expand_path(File.dirname(__FILE__)) + "/lib/assets/cacert.pem"
 
 # set up email
 ActionMailer::Base.delivery_method = :smtp
